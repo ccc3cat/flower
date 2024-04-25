e461c0beb8b72b9d56bc51c52d89e56b072ce376
ccc3cat <162669999+ccc3cat@users.noreply.github.com>
2024-04-25 17:10:01 +0800

    1.0

---
+++ web/JS/script.js
@@ -0,0 +1,27 @@
+$(function(){
+    // 滑動到指定位置
+    $('.menu a').click(function(){
+        var btn = $(this).attr('href');
+        var pos = $(btn).offset();
+        $('html,body').animate({scrollTop:pos.top},1000);
+    });
+
+// 至頂按鈕
+$('#gotop').click(function(){
+    $('html,body').animate({scrollTop:0},1000);
+})
+
+// 淡出淡入
+$(window).scroll(function(){
+    if($(this).scrollTop() > 200 ){
+        $('#gotop').stop().fadeTo('',1);}
+    else{
+        $('#gotop').stop().fadeOut();
+    }
+})
+
+});
+$('.smoove').smoove({
+    offset: '30%',
+
+});
