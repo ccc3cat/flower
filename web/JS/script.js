6f79ffd5db572a347580d4ef017c7c8b76267192
ccc3cat <162669999+ccc3cat@users.noreply.github.com>
2024-04-29 13:48:10 +0800

    4/29-1

--- web/JS/script.js
+++ web/JS/script.js
@@ -1,27 +1,30 @@
-$(function(){
+$(function () {
     // 滑動到指定位置
-    $('.menu a').click(function(){
+    $('.menu a').click(function () {
         var btn = $(this).attr('href');
         var pos = $(btn).offset();
-        $('html,body').animate({scrollTop:pos.top},1000);
+        $('html,body').animate({ scrollTop: pos.top }, 1000);
     });
 
-// 至頂按鈕
-$('#gotop').click(function(){
-    $('html,body').animate({scrollTop:0},1000);
-})
+    // 至頂按鈕
+    $('#gotop').click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 1000);
+    })
 
-// 淡出淡入
-$(window).scroll(function(){
-    if($(this).scrollTop() > 200 ){
-        $('#gotop').stop().fadeTo('',1);}
-    else{
-        $('#gotop').stop().fadeOut();
+    // 淡出淡入
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#gotop').stop().fadeTo('', 1);
+        }
+        else {
+            $('#gotop').stop().fadeOut();
+        }
+    })
+    $('.smoove').smoove({
+        offset: '30%',
+    });
+
+    if ($(window).width() < 821) {
+        $('#about video').remove();
     }
-})
-
-});
-$('.smoove').smoove({
-    offset: '30%',
-
 });
